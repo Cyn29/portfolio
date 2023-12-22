1adcef13c5ff1680794c6637d88194381cf17cd4
Cynthia <cyn2993@gmail.com>
2023-12-22 14:57:58 +0100

    feat(styles): changing styles in cards

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -1,17 +1,39 @@
 import Card from "react-bootstrap/Card";
 import "./Cards.css";
 import PropTypes from "prop-types";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-function Cards({ image, title, description, link, route }) {
+function Cards({ id, image, title, description, bottomImages, link, route, clickedCard, setClickedCard }) {
+    const handleClick = (e) => {
+        e.stopPropagation();
+        setClickedCard(id);
+    };
+
     return (
-        <Card className="card-section">
-            <Card.Img variant="top" src={image} />
+        <Card 
+            className={`card-section ${clickedCard === id ? 'clicked' : ''}`}
+            onClick={handleClick}
+        >
+            <Card.Img variant="top" src={image} className={`card-image ${clickedCard !== id ? 'unclicked' : ''}`} />
             <Card.Body className="card-body">
                 <Card.Title>{title}</Card.Title>
                 <Card.Text>{description}</Card.Text>
+                {bottomImages && (
+                    <div className="bottom-images-container">
+                        {bottomImages.map((img, index) => (
+                            <img
+                                key={index}
+                                src={img}
+                                alt="Technology icon"
+                                className="bottom-image"
+                            />
+                        ))}
+                    </div>
+                )}
                 <Link to={route}>
-                    <button className="card-button" variant="dark">{link}</button>
+                    <button className="card-button" variant="dark">
+                        {link}
+                    </button>
                 </Link>
             </Card.Body>
         </Card>
@@ -19,10 +41,16 @@ function Cards({ image, title, description, link, route }) {
 }
 
 Cards.propTypes = {
+    id: PropTypes.number.isRequired,
     image: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
+    bottomImages: PropTypes.array.isRequired,
     link: PropTypes.string.isRequired,
+    route: PropTypes.string.isRequired,
+    clickedCard: PropTypes.number,
+    setClickedCard: PropTypes.func.isRequired,
 };
 
 export default Cards;
+
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,56 +1,93 @@
+import React, { useState } from "react";
 import Header from "../../components/navbar/Navbar";
 import Cards from "../../components/cards/Cards";
 import Footer from "../../components/footer/Footer";
 import orange from "../../assets/cards/orange.png";
-import madxtreme from "../../assets/cards/madxtreme.png"
-import atuspies from "../../assets/cards/atuspies.png"
-import pokemon from "../../assets/cards/pokemon.png"
-import sportforall from "../../assets/cards/sportforall.png"
-import "./Home.css"
-
+import madxtreme from "../../assets/cards/madxtreme.png";
+import atuspies from "../../assets/cards/atuspies.png";
+import pokemon from "../../assets/cards/pokemon.png";
+import sportforall from "../../assets/cards/sportforall.png";
+import figma from "../../assets/stack/figma.png";
+import css from "../../assets/stack/css.png";
+import react from "../../assets/stack/react.png";
+import nodejs from "../../assets/stack/node.png";
+import typescript from "../../assets/stack/typescript.png";
+import "./Home.css";
 
 function Home() {
+    const [clickedCard, setClickedCard] = useState(null);
+
+    const handleContainerClick = () => {
+        setClickedCard(null); 
+    };
+
     return (
-        <div className="home-container">
-        <Header />
-        <div className="title-container">
-            <h1 className="projects-title">!PROYECTOS</h1>
-        </div>
-        <div className="cards-container">
-        <Cards 
-        image={orange}
-        title="Orange Digital Center"
-        description="Diseño y desarrollo de la interfaz del Orange Digital Center de Madrid."
-        link="Ver más"
-        route="/orangedigitalcenter" />
-        <Cards 
-        image={madxtreme}
-        title="Madxtreme"
-        description="Web de reservas de deportes de aventura en la comunidad de Madrid."
-        link="Ver más"
-        route="/madxtreme" />
-        <Cards
-        image={atuspies}
-        title="A tus pies"
-        description="Dashboard del administrador de un ecommerce de zapatillas deportivas."
-        link="Ver más"
-        route="/atuspies" />
-        <Cards 
-        image={pokemon}
-        title="Catch a coder"
-        description="App en la que los docentes pueden seleccionar alumnos de forma aleatoria."
-        link="Ver más"
-        route="/catchacoder" />
-        <Cards 
-        image={sportforall}
-        title="Sport for all"
-        description="Diseño y maquetación de la landing page de una consultora tecnológica."
-        link="Ver más"
-        route="/sportforall" />
-        </div>
-        <Footer />
+        <div className="home-container" onClick={handleContainerClick}>
+            <Header />
+            <div className="title-container">
+                <h1 className="projects-title">!PROYECTOS</h1>
+            </div>
+            <div className="cards-container">
+                <Cards
+                    id="1"
+                    image={madxtreme}
+                    title="Madxtreme"
+                    description="Web de reservas de deportes de aventura en la comunidad de Madrid. El diseño de la web se realizó en Figma y el desarrollo de la misma se hizo con React Bootstrap, React y Node.js"
+                    bottomImages={[figma, react, nodejs]}
+                    link="Ver más"
+                    route="/madxtreme"
+                    clickedCard={clickedCard}
+                    setClickedCard={setClickedCard}
+                />
+                <Cards
+                    id="2"
+                    image={orange}
+                    title="Orange Digital Center"
+                    description="Diseño y desarrollo de la interfaz del Orange Digital Center de Madrid. El diseño de la interfaz se realizó en Figma. El frontend está programado con CSS y React y el backend con TypeScript."
+                    bottomImages={[figma, css, react, typescript]}
+                    link="Ver más"
+                    route="/orangedigitalcenter"
+                    clickedCard={clickedCard}
+                    setClickedCard={setClickedCard}
+                />
+                <Cards
+                    id="3"
+                    image={atuspies}
+                    title="A tus pies"
+                    description="Dashboard del administrador de un ecommerce de zapatillas deportivas. El backend del proyecto está generado con json-server y el frontend con React y React Bootstrap."
+                    link="Ver más"
+                    bottomImages={[figma, react, nodejs]}
+                    route="/atuspies"
+                    clickedCard={clickedCard}
+                    setClickedCard={setClickedCard}
+                />
+                <Cards
+                    id="4"
+                    image={pokemon}
+                    title="Catch a coder"
+                    description="App en la que los docentes pueden seleccionar alumnos de forma aleatoria. El juego fue diseñado en Figma y a través de HTML, CSS y Javascript fueron creados los estilos y la funcionalidad."
+                    bottomImages={[figma, css, react, typescript]}
+                    link="Ver más"
+                    route="/catchacoder"
+                    clickedCard={clickedCard}
+                    setClickedCard={setClickedCard}
+                />
+                <Cards
+                    id="5"
+                    image={sportforall}
+                    title="Sport for all"
+                    description="Diseño y maquetación de la landing page de una consultora tecnológica. La landing page se diseñó en Figma y se maquetó con HTML y CSS. Se usó JavaScript para añadir funcionalidad a la navbar."
+                    bottomImages={[figma, css, react, typescript]}
+                    link="Ver más"
+                    route="/sportforall"
+                    clickedCard={clickedCard}
+                    setClickedCard={setClickedCard}
+                />
+            </div>
+            <Footer />
         </div>
     );
 }
 
 export default Home;
+
